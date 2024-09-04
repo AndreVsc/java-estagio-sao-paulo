d7b924c5869d4710e3240df16a9c2137fe9f87dd
André Victor Soares Castro <95365578+AndreVsc@users.noreply.github.com>
2024-09-04 00:33:01 -0300

    done

--- src/main/java/andrevsc/quests/FaturamentoDiario.java
+++ src/main/java/andrevsc/quests/FaturamentoDiario.java
@@ -1,5 +1,4 @@
 package andrevsc.quests;
-
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import java.io.BufferedReader;
@@ -7,20 +6,15 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.List;
-
 public class FaturamentoDiario {
-
     private List<Faturamento> faturamentos;
 
     public FaturamentoDiario() {
         carregarDados();
     }
-
     private void carregarDados() {
         String caminhoArquivo = "src/main/java/andrevsc/data/dados.json";
-
         try (BufferedReader reader = new BufferedReader(new FileReader(caminhoArquivo))) {
-            // Usando Gson para fazer o parsing do JSON
             Gson gson = new Gson();
             Type faturamentoListType = new TypeToken<List<Faturamento>>() {}.getType();
             faturamentos = gson.fromJson(reader, faturamentoListType);
@@ -28,14 +22,11 @@ public class FaturamentoDiario {
             System.out.println("Erro ao ler o arquivo: " + e.getMessage());
         }
     }
-
     public void calcularFaturamento() {
         double menorValor = Double.MAX_VALUE;
         double maiorValor = Double.MIN_VALUE;
         double soma = 0;
         int diasComFaturamento = 0;
-
-        // Processando os valores
         for (Faturamento faturamento : faturamentos) {
             double valor = faturamento.getValor();
             if (valor > 0) {
@@ -49,17 +40,13 @@ public class FaturamentoDiario {
                 diasComFaturamento++;
             }
         }
-
         double mediaMensal = soma / diasComFaturamento;
         int diasAcimaDaMedia = 0;
-
-        // Contando dias com faturamento acima da média
         for (Faturamento faturamento : faturamentos) {
             if (faturamento.getValor() > mediaMensal) {
                 diasAcimaDaMedia++;
             }
         }
-
         System.out.println("Menor valor de faturamento: " + menorValor);
         System.out.println("Maior valor de faturamento: " + maiorValor);
         System.out.println("Número de dias com faturamento acima da média mensal: " + diasAcimaDaMedia);
