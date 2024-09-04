04b9f2a713bfc3afdbcfe51e31a77300dbf20236
André Victor Soares Castro <95365578+AndreVsc@users.noreply.github.com>
2024-09-04 00:37:05 -0300

    done

--- src/test/java/andrevsc/AppTest.java
+++ src/test/java/andrevsc/AppTest.java
@@ -1,20 +1,11 @@
 package andrevsc;
 
 import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
     @Test
-    public void shouldAnswerWithTrue()
-    {
+    public void shouldAnswerWithTrue(){
         assertTrue( true );
     }
 }
