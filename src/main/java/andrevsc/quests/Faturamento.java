b050a997b52733a736950e6791f63078072c1f22
André Victor Soares Castro <95365578+AndreVsc@users.noreply.github.com>
2024-09-04 00:31:20 -0300

    Update Faturamento.java

--- src/main/java/andrevsc/quests/Faturamento.java
+++ src/main/java/andrevsc/quests/Faturamento.java
@@ -1,18 +1,15 @@
 package andrevsc.quests;
-
 public class Faturamento {
     private int dia;
     private double valor;
-
+    
     public Faturamento(int dia, double valor) {
         this.dia = dia;
         this.valor = valor;
     }
-
     public int getDia() {
         return dia;
     }
-
     public double getValor() {
         return valor;
     }
